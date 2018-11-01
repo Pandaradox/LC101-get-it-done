d7b86c25c27b05cd1dc0113ea5c81cb438246d25
Pandaradox <jacardarella@gmail.com>
2018-11-01 14:28:28 -0500

    Init main.py and todos.html

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, request, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        tasks.append(request.form['task'])
+    return render_template('todos.html', tasks=tasks)
+
+app.run()
